a9aa84ccb7faaaf943427513413e0ecddc8d98d5
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2024-03-29 23:06:39 +0530

    chore: remove unused function

--- cmplx/heapsort/heapsort.cpp
+++ cmplx/heapsort/heapsort.cpp
@@ -13,12 +13,9 @@ namespace cmplx::heapsort {
 void printHello() {
   std::cout << "Hello!" << std::endl;
 }
-template<typename T>
+s template<typename T>
 void sort(T *Arr, int Length) {
   auto Heap = new cmplx::heapsort::MaxHeap(Arr, Length);
   Heap->heapSort;
-}
-int getMeaningOfLife() {
-  return 42;
 }
 }// namespace cmplx::heapsort
