334d012aba50eb72a0aec662118293969fcb9c00
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2024-03-29 22:17:54 +0530

    refactor: make heapsort generic

--- cmplx/heapsort/heap.h
+++ cmplx/heapsort/heap.h
@@ -1,20 +1,32 @@
-//===-- tests/heap_tests.cpp - Heapsort Implementation -------------------===//
+//===-- tests/heap_tests.cpp - Heapsort Implementation ----------------------------===//
 ///
 /// \file
-/// This file contains the declaration for the Heapsort Data structure.
+/// This file contains the generic implementation for the Heapsort Data structure.
 ///
-//===--------------------------------------------------------------------===//
+//===------------------------------------------------------------------------------===//
 
 #pragma once
 
+#include <cassert>
+#include <math.h>
+
 namespace cmplx::heapsort {
+template<class T>
 class MaxHeap {
 public:
-  int *HeapArr;
+  T *HeapArr;
   int Length;
   int HeapSize;
-  MaxHeap(int *, int);
-  // inline functions need to be declared here due to compiler issues
+
+  MaxHeap(T *RawArr, int Length) {
+	// ensuring that the first element is blank
+	// need to have an array of pointers to complex numbers
+	assert(RawArr[0] == 0);
+	this->HeapArr = RawArr;
+	this->Length = Length;
+	this->buildMaxHeap();
+  }
+
   inline int parent(int Idx) {
 	return Idx >> 1;
   }
@@ -25,9 +37,37 @@ public:
 	return (Idx << 1) + 1;
   };
 
-  void maxHeapify(int);
-  void buildMaxHeap();
-  void heapSort();
+  void maxHeapify(int Root) {
+	auto Left = this->left(Root);
+	auto Right = this->right(Root);
+	int Largest;
+
+	if (Left <= this->HeapSize && this->HeapArr[Left] > this->HeapArr[Root])
+	  Largest = Left;
+	else
+	  Largest = Root;
+	if (Right <= this->HeapSize && this->HeapArr[Right] > this->HeapArr[Largest])
+	  Largest = Right;
+	if (Largest != Root) {
+	  exchange(Root, Largest);
+	  this->maxHeapify(Largest);
+	}
+  }
+
+  void buildMaxHeap() {
+	// 1 less than length since first element is discarded
+	this->HeapSize = this->Length - 1;
+	for (int Idx = floor(this->Length / 2); Idx > 0; Idx--)
+	  this->maxHeapify(Idx);
+  }
+
+  void heapSort() {
+	for (int idx = this->Length - 1; idx >= 2; idx--) {
+	  this->exchange(1, idx);
+	  this->HeapSize--;
+	  this->maxHeapify(1);
+	}
+  }
 
 private:
   // @todo write test case for this function
