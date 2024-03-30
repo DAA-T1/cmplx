ecdfd3435ef8fd08dd985486d4d435c8b9de5d56
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-03-31 00:17:21 +0530

    make quicksort generic

--- cmplx/quicksort/quicksort.cpp
+++ cmplx/quicksort/quicksort.cpp
@@ -1,52 +1,7 @@
-//===-- cmplx/quicksort/quicksort.cpp - quicksort Implementation -----------===//
+//===-- cmplx/quicksort/quicksort.cpp - Dummy CMake Link File -----------===//
 ///
 /// \file
-/// This file contains the implementation of the quicksort algorithm.
-///
-//===--------------------------------------------------------------------===//
+/// This file exists to fix link language issues from CMake.
+/// ===--------------------------------------------------------------------===//
 
 #include <cmplx/quicksort/quicksort.h>
-
-using namespace cmplx::quicksort;
-
-template<typename T>
-void swap(T *A, T *B) {
-  auto Temp = *A;
-  *A = *B;
-  *B = Temp;
-}
-
-int partition(std::vector<int> &Vec, int Left, int Right) {
-  int Pivot = Vec[Left];
-  int I = Left - 1;
-  int J = Right + 1;
-
-  while (true) {
-	do {
-	  I++;
-	} while (Vec[I] < Pivot);
-
-	do {
-	  J--;
-	} while (Vec[J] > Pivot);
-
-	if (I >= J) {
-	  return J;
-	}
-	swap(&Vec[I], &Vec[J]);
-  }
-  // unreachable
-}
-
-void qSort(std::vector<int> &Vec, int Left, int Right) {
-  if (Left >= Right) {
-	return;
-  }
-  int Middle = partition(Vec, Left, Right);
-  qSort(Vec, Left, Middle);
-  qSort(Vec, Middle + 1, Right);
-}
-
-void cmplx::quicksort::sort(std::vector<int> &Vec) {
-  qSort(Vec, 0, Vec.size() - 1);
-}
--- cmplx/quicksort/quicksort.h
+++ cmplx/quicksort/quicksort.h
@@ -10,5 +10,48 @@
 #include <vector>
 
 namespace cmplx::quicksort {
-void sort(std::vector<int> &);
+template<typename T>
+void swap(T *A, T *B) {
+  auto Temp = *A;
+  *A = *B;
+  *B = Temp;
+}
+
+template<typename T>
+int partition(std::vector<T> &Vec, int Left, int Right) {
+  int Pivot = Vec[Left];
+  int I = Left - 1;
+  int J = Right + 1;
+
+  while (true) {
+    do {
+      I++;
+    } while (Vec[I] < Pivot);
+
+    do {
+      J--;
+    } while (Vec[J] > Pivot);
+
+    if (I >= J) {
+      return J;
+    }
+    swap(&Vec[I], &Vec[J]);
+  }
+  // unreachable
+}
+
+template<typename T>
+void qSort(std::vector<T> &Vec, int Left, int Right) {
+  if (Left >= Right) {
+    return;
+  }
+  int Middle = partition(Vec, Left, Right);
+  qSort(Vec, Left, Middle);
+  qSort(Vec, Middle + 1, Right);
+}
+
+template<typename T>
+void sort(std::vector<T> &Vec) {
+  qSort(Vec, 0, Vec.size() - 1);
+}
 }// namespace cmplx::quicksort
