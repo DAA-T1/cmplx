33c33a915ea6344b0ce97f81b266d9d4d69f2cdd
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2024-03-30 16:53:55 +0530

    use qualified names instead of using the namespace in tests

--- tests/quicksort_test.cpp
+++ tests/quicksort_test.cpp
@@ -2,31 +2,30 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-using namespace cmplx::quicksort;
-
 TEST(QuickSortTest, TestSingleElement) {
   std::vector<int> V{42};
-  sort(V);
+  cmplx::quicksort::sort(V);
 
   EXPECT_EQ(V[0], 42) << "the element is not 42";
 }
 
 TEST(QuickSortTest, TestTwoElements) {
   std::vector<int> V{42, -42};
-  sort(V);
+  cmplx::quicksort::sort(V);
 
   ASSERT_THAT(V, testing::ElementsAre(-42, 42));
 }
 
 TEST(QuickSortTest, TestThreeElements) {
   std::vector<int> V{0, 42, -42};
-  sort(V);
+  cmplx::quicksort::sort(V);
 
   ASSERT_THAT(V, testing::ElementsAre(-42, 0, 42));
 }
 
 TEST(QuickSortTest, TestForReal) {
   std::vector<int> V{7, 2, -12, 13, 42, 21, -42, 0};
-  sort(V);
+  cmplx::quicksort::sort(V);
+
   ASSERT_THAT(V, testing::ElementsAre(-42, -12, 0, 2, 7, 13, 21, 42));
 }
