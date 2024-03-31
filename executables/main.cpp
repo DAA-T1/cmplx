fb189d3bf3f85fee0f64735bff38f21f4b561c94
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2024-03-31 14:32:42 +0530

    refactor: add type alias for chrono<double> type

--- executables/main.cpp
+++ executables/main.cpp
@@ -2,16 +2,18 @@
 // Created by jalan on 30-03-2024.
 //
 
-#include <iostream>
-#include <cmplx/heapsort/heapsort.h>
-#include <cmplx/readfile/readfile.h>
-#include <cmplx/quicksort/quicksort.h>
-#include <cmplx/utils/utils.h>
-#include <cmplx/utils/complex.h>
-#include <string.h>
 #include <chrono>
+#include <cmplx/heapsort/heapsort.h>
+#include <cmplx/quicksort/quicksort.h>
+#include <cmplx/readfile/readfile.h>
+#include <cmplx/utils/complex.h>
+#include <cmplx/utils/utils.h>
+#include <iostream>
+#include <string.h>
 
-int main(int Argc, char* Argv[]) {
+using double_time = std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>>;
+
+int main(int Argc, char *Argv[]) {
   if (Argc < 2) {
 	std::cerr << "Usage: " << Argv[0] << " --heap/--quick --time --file <filename>" << std::endl;
 	exit(1);
@@ -22,29 +24,25 @@ int main(int Argc, char* Argv[]) {
   for (int I = 1; I < Argc; I++) {
 	if (!strcmp(Argv[I], "--heap")) {
 	  Sort = "heap";
-	}
-	else if (!strcmp(Argv[I], "--quick")) {
+	} else if (!strcmp(Argv[I], "--quick")) {
 	  Sort = "quick";
-	}
-	else if (!strcmp(Argv[I], "--time")) {
+	} else if (!strcmp(Argv[I], "--time")) {
 	  Time = true;
-	  }
-	  else if (!strcmp(Argv[I], "--file")) {
-		FileName = Argv[++I];
-	  }
+	} else if (!strcmp(Argv[I], "--file")) {
+	  FileName = Argv[++I];
+	}
   }
   cmplx::utils::ComplexNumber *Arr;
   int N;
   cmplx::readfile::readFromFile(FileName, &Arr, N);
 
-  std::chrono::time_point<std::chrono::system_clock> Start;
-  std::chrono::time_point<std::chrono::system_clock> Stop;
+  double_time Start;
+  double_time Stop;
   if (Sort == "heap") {
 	Start = std::chrono::high_resolution_clock::now();
 	cmplx::heapsort::sort(Arr, N);
 	Stop = std::chrono::high_resolution_clock::now();
-  }
-  else if (Sort == "quick") {
+  } else if (Sort == "quick") {
 	Start = std::chrono::high_resolution_clock::now();
 	cmplx::quicksort::sort(Arr, N);
 	Stop = std::chrono::high_resolution_clock::now();
@@ -55,11 +53,8 @@ int main(int Argc, char* Argv[]) {
   std::cout << "Sorted Array: ";
   cmplx::utils::printArray(Arr, N);
   if (Time) {
-  std::cout << "Duration (in nanoseconds): " << Duration.count() << std::endl;
+	std::cout << "Duration (in nanoseconds): " << Duration.count() << std::endl;
 
-  delete Arr;
-
+	delete Arr;
   }
-
-
 }
