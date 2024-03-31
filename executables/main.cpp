942ce8345e6f34fdc4c76bd01f7330bb637b6fac
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2024-03-31 15:08:51 +0530

    fix: do not set double
    Co-authored-by: Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>

--- executables/main.cpp
+++ executables/main.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 #include <string.h>
 
-using double_time = std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>>;
+using double_time = std::chrono::time_point<std::chrono::steady_clock>;
 
 int main(int Argc, char *Argv[]) {
   if (Argc < 2) {
